c9bd2dccf333a1ebbd2553c229c99a1ce6feb3d6
Oleksandr Brui <oleksandr.bruigit@gmail.com>
2021-03-14 20:35:32 +0200

    Solution

--- js_arrayMethodPush.js
+++ js_arrayMethodPush.js
@@ -1,1 +1,12 @@
 'use strict';
+
+numbers.push = function(...values) {
+  const { length: arrayLength } = this;
+  const { length: valuesLength } = values;
+
+  for (let i = 0; i < valuesLength; i += 1) {
+    this[arrayLength + i] = values[i];
+  }
+
+  return this.length;
+};
